8efc1c02b28eee7127b6992962ff4c9f3a412c86
Liu Jun <liujun@google.com>
2010-05-10 14:10:48 +0800

    Initialize the muerrrrr Git repository.

---
+++ django-nonrel/build/lib/django/utils/module_loading.py
@@ -0,0 +1,26 @@
+import os
+import imp
+
+def module_has_submodule(mod, submod_name):
+    # If the module was loaded from an egg, __loader__ will be set and
+    # its find_module must be used to search for submodules.
+    loader = getattr(mod, '__loader__', None)
+    if loader:
+        mod_path = "%s.%s" % (mod.__name__.rsplit('.',1)[-1], submod_name)
+        x = loader.find_module(mod_path)
+        if x is None:
+            # zipimport.zipimporter.find_module is documented to take
+            # dotted paths but in fact through Python 2.7 is observed
+            # to require os.sep in place of dots...so try using os.sep
+            # if the dotted path version failed to find the requested
+            # submodule.
+            x = loader.find_module(mod_path.replace('.', os.sep))
+        return x is not None
+
+    try:
+        imp.find_module(submod_name, mod.__path__)
+        return True
+    except ImportError:
+        return False
+
+
---
+++ django-testapp/settings.py
@@ -0,0 +1,16 @@
+from djangoappengine.settings_base import *
+
+SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
+
+INSTALLED_APPS = (
+    'djangoappengine',
+    'djangotoolbox',
+#    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+)
+
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+import os
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
